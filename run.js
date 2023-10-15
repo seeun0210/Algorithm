1df05bd6b72dcc8413b2027fd096ec756b635f44
KimSeEun <cocobell3@naver.com>
2023-10-15 20:08:09 +0900

    study: 달리기 경주

---
+++ run.js
@@ -0,0 +1,41 @@
+function solution(players, callings) {
+  const playerMap = {}; // 플레이어 이름을 인덱스로 매핑
+  for (let i = 0; i < players.length; i++) {
+    playerMap[players[i]] = i;
+  }
+
+  for (let i = 0; i < callings.length; i++) {
+    const calling = callings[i];
+    if (playerMap[calling] !== undefined) {
+      const callIdx = playerMap[calling];
+      if (callIdx > 0) {
+        // 스왑 연산 수행
+        [players[callIdx], players[callIdx - 1]] = [
+          players[callIdx - 1],
+          players[callIdx],
+        ];
+        playerMap[calling] = callIdx - 1;
+        playerMap[players[callIdx]] = callIdx;
+      }
+    }
+  }
+
+  return players;
+}
+
+const players = ["mumu", "soe", "poe", "kai", "mine"];
+const callings = ["kai", "kai", "mine", "mine"];
+console.log(solution(players, callings));
+
+// [처음 풀이]
+// function solution(players, callings) {
+//     for (let i = 0; i < callings.length; i++) {
+//       const callidx = players.indexOf(callings[i]);
+//       if (callidx !== -1 && callidx > 0) {
+//         const temp = players[callidx];
+//         players[callidx] = players[callidx - 1];
+//         players[callidx - 1] = temp;
+//       }
+//     }
+//     return players;
+//   }
